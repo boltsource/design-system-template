289983232e32430ce14b2a4748005290a7bc7971
Kevin Rodriguez <_@kevinrodriguez.io>
2020-08-17 21:10:08 -0600

    Checkpoint #3

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,17 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Alert } from "react-native";
-import { XPButton } from "@xproduct/ui";
+import { XPButton, XPCard } from "@xproduct/ui";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <XPButton
-        title="Welcome"
-        onPress={() => {
-          Alert.alert("Hi", "This is a native alert");
-        }}
-      />
+      <XPCard>
+        <Text>
+          Ipsum ad deserunt consectetur laborum esse anim cillum anim tempor
+          nulla in enim. Sunt pariatur incididunt amet eu dolor irure
+          consectetur voluptate id enim aliquip est voluptate Lorem. Aliqua
+          laboris deserunt officia reprehenderit mollit tempor consectetur nulla
+          laborum mollit labore ut. Fugiat duis dolor est eu laborum enim sint
+          voluptate anim labore sit do ad consequat. Lorem cillum labore ad
+          adipisicing. Irure est amet nostrud esse excepteur. Ea ad laborum nisi
+          id est excepteur eu labore laboris culpa cillum.
+        </Text>
+        <View style={{ margin: 16 }} />
+        <XPButton title="Action" onPress={() => {}} />
+      </XPCard>
       <StatusBar style="auto" />
     </View>
   );
--- apps/site/src/index.tsx
+++ apps/site/src/index.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Text, View, StyleSheet } from "react-native";
+import { XPCard, XPButton } from "@xproduct/ui";
+import './styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    height: '100%'
+  },
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <XPCard>
+        <Text>
+          Ipsum ad deserunt consectetur laborum esse anim cillum anim tempor
+          nulla in enim. Sunt pariatur incididunt amet eu dolor irure
+          consectetur voluptate id enim aliquip est voluptate Lorem. Aliqua
+          laboris deserunt officia reprehenderit mollit tempor consectetur nulla
+          laborum mollit labore ut. Fugiat duis dolor est eu laborum enim sint
+          voluptate anim labore sit do ad consequat. Lorem cillum labore ad
+          adipisicing. Irure est amet nostrud esse excepteur. Ea ad laborum nisi
+          id est excepteur eu labore laboris culpa cillum.
+        </Text>
+        <View style={{ margin: 16 }} />
+        <XPButton title="Action" onPress={() => {}} />
+      </XPCard>
+    </View>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
